081606b481d1ef27dab8e0f2cc3a503c1e3ce436
Adam Walter <adam@vtldesign.com>
2020-04-20 15:27:23 -0400

    Adds missing items to header comment

--- wp-plugin-boilerplate.php
+++ wp-plugin-boilerplate.php
@@ -4,25 +4,11 @@
 	Plugin URI:
 	Description:
 	Version: 1.0
+	Requires at least: 5.2
+	Requires PHP: 7.0
 	Author: Vital
 	Author URI: https://vtldesign.com
 	Text Domain: vital
-	License: GPLv2
-
-	Copyright 2020  VITAL  (email : hello@vtldesign.com)
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 2, as
-	published by the Free Software Foundation.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 if (! defined('ABSPATH')) {
